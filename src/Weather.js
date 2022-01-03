9e325aed3bf89b518f7b13134d989aa1cd54c2e0
DeyaSHTA93 <93060098+DeyaSHTA93@users.noreply.github.com>
2022-01-03 21:56:39 +0100

    corrections
    removed line 2 on App.js and remove line 3 on Weather.js / Also changed alert with quotes to backtick

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,10 @@
 import React from "react";
 import axios from "axios";
-import App from "./App";
 
 export default function Weather(props) {
   function handleResponse(response) {
     alert(
-      "The weather in ${response.data.name} is ${response.data.main.temp}°C"
+      `The weather in ${response.data.name} is ${response.data.main.temp}°C`
     );
   }
   let apiKey = "ce80792f848dcdfca4fa748e5d4f8f19";
